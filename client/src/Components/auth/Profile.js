81030a5096568d4f06ebdb080b1b663206941b6e
MikoMagritto <77416124+MikoMagritto@users.noreply.github.com>
2021-07-25 22:20:19 +0200

    logout

--- client/src/Components/auth/Profile.js
+++ client/src/Components/auth/Profile.js
@@ -1,104 +1,65 @@
-import React, { Component } from 'react'
-import service from './auth-service';
+import React, { Component } from "react";
+import service from "./auth-service";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class Profile extends Component {
+  logout = (event) => {
+    axios.post('http://localhost:5000/auth/logout', {})
+    .then((response) => {
+      this.props.updateUser(false);
+    });
+  };
 
-    logout = (event) => {
-        service.logout()
-            .then(response => {
-                this.props.updateUser(false);
-            });
-    }
+  handleUpload = (event) => {
+    let formData = new FormData();
+    formData.append("photo", event.target.files[0]);
 
-    handleUpload = (event) => {
-        let formData = new FormData();
-        formData.append('photo', event.target.files[0]);
+    service.upload(formData).then((response) => {
+      this.props.updateUser(response);
+    });
+  };
 
-        service.upload(formData)
-            .then(response => {
-                this.props.updateUser(response);
-            });
-    }
+  render() {
+    return (
+      // <Link to='/logout'>Se déconnecter</Link>
 
-
-
-    render() {
-
-
-        return (
-            // <Link to='/logout'>Se déconnecter</Link>
-
-
-            <
-            div >
-            <
-            h1 > Profile < /h1>
-
-            <
-            p >
-            <
-            h3 > Username < /h3><br/ >
-            <
-            span > { this.props.user.data.username } < /span> </p >
-            <
-            p >
-            <
-            h3 > Height < /h3>  <
-            span > { this.props.user.data.height } < /span> </p >
-            <
-            p >
-            <
-            h3 > Age < /h3>  <
-            span > { this.props.user.data.age } < /span>  <
-            /p>
-
-            <
-            p >
-            <
-            h3 > email < /h3>  <
-            span > { this.props.user.data.email } < /span>  <
-            /p>
-
-            <
-            p >
-            <
-            h3 > Level < /h3>  <
-            span > { this.props.user.data.level } < /span>  <
-            /p>
-
-            <
-            p >
-            <
-            h3 > Profil Pic < /h3>  <
-            img src = { this.props.user.data.avatar }
-            />  <
-            /p>
-
-            {
-                /* <p>
+      <div>
+        <h1> Profile </h1>
+        <p>
+          <h3> Username </h3>
+          <br />
+          <span> {this.props.user.data.username} </span>
+        </p>
+        <p>
+          <h3> Height </h3> <span> {this.props.user.data.height} </span>
+        </p>
+        <p>
+          <h3> Age </h3> <span> {this.props.user.data.age} </span>
+        </p>
+        <p>
+          <h3> email </h3> <span> {this.props.user.data.email} </span>
+        </p>
+        <p>
+          <h3> Level </h3> <span> {this.props.user.data.level} </span>
+        </p>
+        <p>
+          <h3> Profil Pic </h3> <img src={this.props.user.data.avatar} />
+        </p>
+        {/* <p>
                             <label>
                               <h3>Avatar</h3>
                               <input type="file" onChange={(e) => this.fileChange(e)} />
                             </label>
-                          </p> */
-            }
-
-            <
-            div className = "cta" >
-            <
-            button className = "btn logout"
-            onClick = { this.logout } > Logout < /button>  <
-            /div>
-
-            {
-                /* <form>
-                              <label>
-                                <img className="avatar" src={this.props.user.image || "https://material.io/tools/icons/static/icons/baseline-person-24px.svg"} />
-                                <input type="file" name="image" onChange={this.handleUpload} />
-                              </label>
-                              
-                            </form> */
-            } < /div>
-        )
-    }
+                          </p> */}
+        <div className="cta">
+          <Link to='/'>
+            <button className="btn logout" onClick={this.logout}>
+              Logout
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
