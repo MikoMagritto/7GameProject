a3585a51d60fca0287fd433c36c3c7bb231948c5
MikoMagritto <77416124+MikoMagritto@users.noreply.github.com>
2021-07-27 21:35:37 +0200

    elodie

--- client/src/Components/ListGame.js
+++ client/src/Components/ListGame.js
@@ -1,6 +1,14 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class ListGame extends Component {
+state ={
+    games: []
+}
+
+
+
+
     render() {
         return (
             <div>
--- routes/games-routes.js
+++ routes/games-routes.js
@@ -2,26 +2,30 @@ const express = require("express");
 const Game = require("../models/Game");
 const gamesRoutes = express.Router();
 
-
 gamesRoutes.get("/", (req, res, next) => {
   Game.find()
-    .then(AllGamesFromDb => {
-      console.log("AllGamesFromDb", AllGamesFromDb)
+    .then((AllGamesFromDb) => {
+      console.log("AllGamesFromDb", AllGamesFromDb);
       res.json(AllGamesFromDb);
     })
-    .catch(err => console.log(err))
-})
+    .catch((err) => console.log(err));
+});
 
 gamesRoutes.post("/add", (req, res, next) => {
   const organisator = req.session.currentUser;
-  const {
-    name, players, numPlayers, levelGame,
-    field, mood, date, typeGame } = req.body;
+  const { name, players, numPlayers, levelGame, field, mood, date, typeGame } =
+    req.body;
   const data = {
-    name, players, numPlayers, levelGame,
-    field, mood, date, typeGame
+    name,
+    players,
+    numPlayers,
+    levelGame,
+    field,
+    mood,
+    date,
+    typeGame,
   };
-  console.log('data is : ', data);
+  console.log("data is : ", data);
 
   // if (!name || !levelGame || !field || !mood) {
   //   res.status(400).json({ message: "informations missing" });
@@ -39,27 +43,33 @@ gamesRoutes.post("/add", (req, res, next) => {
     typeGame: typeGame,
     //field: field, ---------> mettre IdField.
     //image, -------> Mettre image du terrain.
-  })
+  });
 
-  aNewGame
-    .save()
-    .then(() => {
-      console.log('newGame', aNewGame)
-      res.status(200).json(aNewGame)
-    })
-
-
-})
+  aNewGame.save().then(() => {
+    console.log("newGame", aNewGame);
+    res.status(200).json(aNewGame);
+  });
+});
 
 gamesRoutes.put("/edit/:id", (req, res, next) => {
   //console.log("editRoad")
   const organisator = req.session.currentUser;
 
-  const { name, players, numPlayers, levelGame, field, mood, date, typeGame } = req.body;
+  const { name, players, numPlayers, levelGame, field, mood, date, typeGame } =
+    req.body;
 
-  const data = { name, players, numPlayers, levelGame, field, mood, date, typeGame };
+  const data = {
+    name,
+    players,
+    numPlayers,
+    levelGame,
+    field,
+    mood,
+    date,
+    typeGame,
+  };
 
-  console.log('data is : ', data);
+  console.log("data is : ", data);
 
   const id = req.params.id;
   //console.log(id);
@@ -71,11 +81,7 @@ gamesRoutes.put("/edit/:id", (req, res, next) => {
   if (req.file) {
     data.avatar = req.file.path;
   }
-  Game.findByIdAndUpdate(
-    { _id: id },
-    data,
-    { new: true }
-  )
+  Game.findByIdAndUpdate({ _id: id }, data, { new: true })
     .then((newGame) => {
       console.log("new user", newGame);
       res.status(200).json(newGame);
@@ -83,9 +89,11 @@ gamesRoutes.put("/edit/:id", (req, res, next) => {
     .catch(next);
 });
 
-gamesRoutes.get('/delete/:id', (req, res) => {
+gamesRoutes.get("/delete/:id", (req, res) => {
   const id = req.params.id;
-  Game.findByIdAndDelete(id).then(() => res.redirect('/')).catch(error => console.log(error));
+  Game.findByIdAndDelete(id)
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
 });
 
 module.exports = gamesRoutes;
